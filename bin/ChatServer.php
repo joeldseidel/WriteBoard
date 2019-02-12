45a769a7ec1236286df591d5e42810505ca356ad
Joel Seidel <jds6294@psu.edu>
2019-02-12 15:50:54 -0500

    updated chat to work through web browser

--- bin/ChatServer.php
+++ bin/ChatServer.php
@@ -1,12 +1,17 @@
 <?php
 use Ratchet\Server\IoServer;
+use Ratchet\Http\HttpServer;
+use Ratchet\WebSocket\WsServer;
 use TeamFour\Chat;
 
 require dirname(__DIR__) . '/vendor/autoload.php';
 
 $server = IoServer::factory(
-    new Chat(),
+    new HttpServer(
+        new WsServer(
+            new Chat()
+        )
+    ),
     8080
 );
-
 $server->run();
