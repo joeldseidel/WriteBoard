9f6d1238ca7f73478c5e17d0349c188a9961a49c
Joel Seidel <jds6294@psu.edu>
2019-02-12 15:38:33 -0500

    basic chat works now

--- src/Chat.php
+++ src/Chat.php
@@ -4,12 +4,28 @@ use Ratchet\MessageComponentInterface;
 use Ratchet\ConnectionInterface;
 
 class Chat implements MessageComponentInterface{
+    protected $clients;
+    public function __construct(){
+        $this->clients = new \SplObjectStorage;
+    }
     public function onOpen(ConnectionInterface $conn){
+        $this->clients->attach($conn);
+        echo "{$conn->resourceId} connected\n";
     }
     public function onMessage(ConnectionInterface $from, $msg){
+        $numRecv = count($this->clients) - 1;
+        echo sprintf('Connection %d sending message "%s" to %d other connections%s' . "\n", $from->resourceId, $msg, $numRecv, $numRecv == 1 ? '' : 's');
+        foreach($this->clients as $client){
+            if($from !== $client){
+                $client->send($msg);
+            }
+        }
     }
     public function onClose(ConnectionInterface $conn){
+        $this->clients->detach($conn);
+        echo "{$conn->resourceId} disconnected\n";
     }
     public function onError(ConnectionInterface $conn, \Exception $e){
+        echo "Error occurred :[   ERROR MESSAGE: {$e->getMessage()}\n";
     }
 }
