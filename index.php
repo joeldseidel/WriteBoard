9a9d8004a908baca168e2a29eadf8e2e2a44ac97
Joel Seidel <jds6294@psu.edu>
2019-02-12 19:05:23 -0500

    set up UI pages

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<head>
+    <title>WriteBoard</title>
+    <meta charset="UTF-8">
+</head>
+<body>
+    <h1>Welcome to WriteBoard</h1>
+    <form id="enterForm">
+        <label for="username">Enter a username:</label>
+        <input type="text" name="username">
+        <input type="submit">
+    </form>
+</body>
