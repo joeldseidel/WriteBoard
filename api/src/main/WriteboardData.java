9c5dc2c2583e7a1037bfd95f952baf0ba4cda063
Joel Seidel <jds6294@psu.edu>
2019-02-21 17:37:01 -0500

    Create API Http server

---
+++ api/src/main/WriteboardData.java
@@ -0,0 +1,33 @@
+package main;
+
+import com.sun.net.httpserver.HttpServer;
+import main.Handlers.AddPathHandler;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
+/**
+ * Author: Joel Seidel / Xuezhou Wen
+ */
+public class WriteboardData {
+    /**
+     * Entry point for the API
+     * Start the HTTP Server listener when the API is started
+     *
+     * @param args because java
+     */
+    public static void main(String args[]){
+        //Create http server
+        try{
+            HttpServer server;
+            server = HttpServer.create(new InetSocketAddress(6869), 0);
+            server.createContext("/add-path", new AddPathHandler());
+            server.start();
+        } catch (IOException ioEx) {
+            //In my years of working with Java HTTP/s, never once have I encountered this
+            //But it's here to shut IntelliJ up
+            ioEx.printStackTrace();
+        }
+        //TODO: start the worker thread to process the queue
+    }
+}
